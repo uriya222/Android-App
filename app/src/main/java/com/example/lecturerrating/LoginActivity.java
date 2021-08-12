c75e0085509e2426958cdf7f0a39c8f826d55637
uriya <uriya@gmail.com>
2021-08-12 23:35:30 +0300

    register and login xml

---
+++ app/src/main/java/com/example/lecturerrating/LoginActivity.java
@@ -0,0 +1,22 @@
+// for converting icon from svg file to xml use this site http://inloop.github.io/svg2android/
+
+
+package com.example.lecturerrating;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TextView txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
